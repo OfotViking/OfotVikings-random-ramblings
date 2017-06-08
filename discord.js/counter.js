8045540e96d6bc70c5d5362b8f84f30f22b5e1dd
OfotViking <ofotviking@gmail.com>
2017-06-08 23:59:58 +0200

    beercounter

--- discord.js/counter.js
+++ discord.js/counter.js
@@ -1,11 +1,15 @@
-// an attempt to count number of occurrences of a string of text
+// an attempt to count number of occurrences of a string of text, in this example :beer: emoji. Also counts :beer: reactions
 
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var fs = require("fs");
 
+var text_to_look_for = "🍺";
+var get_count_command = "beercount";
+
+
 function MessageCounter(msg, countValue) {
-	msg.channel.sendMessage("Text has now occured " + countValue + " times.");
+	msg.channel.send("**" + countValue + "**" + text_to_look_for + " recieved");
 }
 
 function IncrementCounter(msg, countValue) {
@@ -13,10 +17,73 @@ function IncrementCounter(msg, countValue) {
 		if (error) {
 			console.error("write error:  " + error.message);
 			} else {
-			MessageCounter(msg, countValue);
+			//MessageCounter(msg, countValue);
+			console.log(get_count_command + " = " + countValue);
 		}
 	});
 }
+
+function IncrementCounterReaction(countValue) {
+	fs.writeFile("./counter.txt", countValue, function read (error) {
+		if (error) {
+			console.error("write error:  " + error.message);
+			} else {
+			console.log(get_count_command + " = " + countValue);
+		}
+	});
+}
+
+
+
+bot.on("messageReactionAdd", (reaction, user) => {
+	if (reaction.emoji.name == text_to_look_for) {
+		fs.access("./counter.txt", (err) => {
+			if (!err) {
+				//console.log('counting file exists. read the content, increment the value and write back the file');
+				fs.readFile("./counter.txt", "utf8", function read (error, countValue) {
+					if (error) {
+						console.error("read error:  " + error.message);
+						} else {
+						var countValue = Number(countValue)
+						countValue++;
+						IncrementCounterReaction(countValue);
+					}
+				});
+			} else {
+				//console.log('counting file does not exist, first run? set counter to 0 and write the file');
+				var countValue = 0
+				IncrementCounterReaction(countValue);
+			}
+		});
+	};
+});
+
+bot.on("messageReactionRemove", (reaction, user) => {
+	if (reaction.emoji.name == text_to_look_for) {
+		fs.access("./counter.txt", (err) => {
+			if (!err) {
+				//console.log('counting file exists. read the content, increment the value and write back the file');
+				fs.readFile("./counter.txt", "utf8", function read (error, countValue) {
+					if (error) {
+						console.error("read error:  " + error.message);
+						} else {
+						var countValue = Number(countValue)
+						countValue--;
+						IncrementCounterReaction(countValue);
+					}
+				});
+			} else {
+				//console.log('counting file does not exist, first run? set counter to 0 and write the file');
+				var countValue = 0
+				IncrementCounterReaction(countValue);
+			}
+		});
+	};
+});
+
+
+
+
 
 bot.on("message", msg => {
 	// Ignore DM
@@ -30,7 +97,10 @@ bot.on("message", msg => {
 
 	var input = msg.content.toLowerCase();
 	
-	if (input.includes("count occurrences of this text")) {
+	if (input.includes(text_to_look_for)) {
+		var re = new RegExp(text_to_look_for, 'g');
+		var count = (input.match(re) || []).length;
+		console.log("+ " + count);
 		fs.access("./counter.txt", (err) => {
 			if (!err) {
 				//console.log('counting file exists. read the content, increment the value and write back the file');
@@ -38,8 +108,7 @@ bot.on("message", msg => {
 					if (error) {
 						console.error("read error:  " + error.message);
 						} else {
-						var countValue = Number(countValue);
-						countValue++;
+						var countValue = Number(countValue) + count;
 						IncrementCounter(msg, countValue);
 					}
 				});
@@ -55,7 +124,7 @@ bot.on("message", msg => {
 	if(!msg.content.startsWith(prefix)) return;
 
 	//display the current counter value with !counter command
-	if (input.startsWith(prefix + "counter")) {
+	if (input.startsWith(prefix + get_count_command)) {
 		fs.readFile("./counter.txt", "utf8", function read (error, countValue) {
 			if (error) {
 				console.error("read error:  " + error.message);
